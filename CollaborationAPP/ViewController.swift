d6dcab20c33dbff4497dce95cb5a0f0d2e33c6a1
Samuel Shokodko 2019 <sshokod9@3012-l29536-s.local>
2019-05-08 10:05:18 -0500

    a little bit more code

--- CollaborationAPP/ViewController.swift
+++ CollaborationAPP/ViewController.swift
@@ -10,20 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hi")
-        // Do any additional setup after loading the view, typically from a nib.
+      
     }
 
-
-    @IBAction func AnswerButtonAct(_ sender: UIButton) {
+  @IBOutlet weak var answerTextViewOutlet: UITextView!
+    @IBAction func AnswerButtonAct(_ sender: UIButton) {// if the user clicks to get an answer
+        if(questiontextFieldOutlet.text != nil){// if the user has asked a question checks to see if the text field has input
+            
+            
+        }
+        else{
+            
+            let alert = UIAlertController(title: "error", message: "please ask a question", preferredStyle: .alert)//
+            let cancel = UIAlertAction(title: "cancel", style: .default, handler: nil)
+           alert.addAction(cancel)
+            self.present(alert, animated: true, completion: nil)
+           
+    }
     }
     @IBAction func questiontextFieldAction(_ sender: UITextField) {
+        
     }
     @IBOutlet weak var ball8Outlet: UIImageView!
     @IBOutlet weak var questiontextFieldOutlet: UITextField!
-    @IBOutlet weak var answerTextViewOutlet: UITextView!
+  
     
     @IBOutlet weak var AnswerButtonOut: UIButton!
 }
