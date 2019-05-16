4e1b02ca557de8d717abba459c9134a6583b0371
Samuel Shokodko 2019 <sshokod9@3012-l29536-s.local>
2019-05-16 09:43:03 -0500

    DONE MINUS COMMENTS

--- CollaborationAPP/Magic8ball.swift
+++ CollaborationAPP/Magic8ball.swift
@@ -10,45 +10,31 @@ import UIKit
 
 class Magic8ball: UIViewController {
 
-    var advice:[String] = ["outlook not so good","my reply is no","nope","my sources say no","very doubtful","dont count on it","absouletly no","better not tell you know","yes","heck-yes","signs point to yes", "outlook good","for sure"]
+    var advice:[String] = [" good","my reply is no","nope","my sources say no","very doubtful","dont count on it","absouletly no","yes","heck-yes","signs point to yes", "outlook good","for sure"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        Imageball.isHidden = true
-         answerTextViewOutlet.isHidden = true
         
-      
     }
 
-  @IBOutlet weak var answerTextViewOutlet: UITextView!
+ 
     @IBAction func AnswerButtonAct(_ sender: UIButton) {// if the user clicks to get an answer
         if(questiontextFieldOutlet.text != nil && questiontextFieldOutlet.text != "" && questiontextFieldOutlet.text != " " ){
             // if the user has asked a question checks to see if the text field has input that is not a space
-            
-            
             let rng = arc4random_uniform(UInt32(0) + UInt32(12))
-            print(rng)
-            
-            Imageball.isHidden = false
-            answerTextViewOutlet.isHidden = false
-            print(advice[Int(rng)])
-            answerTextViewOutlet.text = advice[Int(rng)]
+            answerLabel.text = advice[Int(rng)]
         }
         else{
-            
             let alert = UIAlertController(title: "error", message: "please ask a question", preferredStyle: .alert)//
             let cancel = UIAlertAction(title: "cancel", style: .default, handler: nil)
            alert.addAction(cancel)
             self.present(alert, animated: true, completion: nil)
-           
     }
     }
     @IBAction func questiontextFieldAction(_ sender: UITextField) {
-        
     }
     @IBOutlet weak var ball8Outlet: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var questiontextFieldOutlet: UITextField!
-  
-    
     @IBOutlet weak var Imageball: UIImageView!
     @IBOutlet weak var AnswerButtonOut: UIButton!
 }
